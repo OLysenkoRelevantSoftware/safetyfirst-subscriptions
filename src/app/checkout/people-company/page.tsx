a8c07a07ac0968e138637a204860141960e22c22
OleksandrLysenko <o.lysenko@relevant.software>
2025-12-23 12:44:20 +0100

    Updates

--- src/app/checkout/people-company/page.tsx
+++ src/app/checkout/people-company/page.tsx
@@ -15,6 +15,7 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
+import Divider from "@mui/material/Divider";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
@@ -29,12 +30,14 @@ const prices = {
   monthly: {
     id: process.env.NEXT_PUBLIC_STRIPE_PEOPLE_COMPANY_PRICE_MONTHLY,
     value: 3.49,
-    title: "Monthly (*Min 3-month term)",
+    title: "Monthly",
+    subtitle: "(*Min 3-month term)",
   },
   yearly: {
     id: process.env.NEXT_PUBLIC_STRIPE_PEOPLE_COMPANY_PRICE_YEARLY,
     value: 36.99,
     title: "Annual",
+    subtitle: "",
   },
 };
 
@@ -107,92 +110,102 @@ export default function SmallCompanyPage() {
           sx={{
             my: 4,
             display: "flex",
+            flexDirection: { xs: "column", md: "row" },
             justifyContent: "center",
-            alignItems: "center",
             width: "100%",
             gap: 4,
           }}
         >
-          <Box
-            sx={{
-              mb: 4,
-              maxWidth: 400,
-              width: "100%",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant="h6" component="h2" sx={{ mb: 0.5 }}>
-              Plan consists of:
-            </Typography>
-            <Typography variant="body1" component="div">
-              • DVLA check every 6 months
-              <br />
-              • Risk Assessment
-              <br />• Quarterly eLearning
-            </Typography>
-          </Box>
-
-          <Card sx={{ maxWidth: 500, mb: 4, width: "100%" }}>
+          <Card sx={{ maxWidth: 500, mb: 4, width: "100%", height: "100%" }}>
             <CardContent>
+              <Typography variant="h5" gutterBottom>
+                Plan Summary
+              </Typography>
+              <Divider sx={{ my: 1.5 }} />
+
               <Typography
                 variant="h6"
                 component="h2"
                 sx={{ mb: 0.5, textAlign: "center" }}
               >
+                Plan consists of:
+              </Typography>
+              <Typography variant="body1" component="div">
+                • DVLA check every 6 months
+                <br />
+                • Risk Assessment
+                <br />• Quarterly eLearning
+              </Typography>
+
+              <Typography
+                variant="h6"
+                component="h2"
+                sx={{ mt: 4, mb: 0.5, textAlign: "center" }}
+              >
                 Select preferable plan
               </Typography>
-              <Box sx={{ mb: 3 }}>
-                <RadioGroup
-                  row
-                  value={period}
-                  onChange={(e) =>
-                    setPeriod(e.target.value as "monthly" | "yearly")
-                  }
-                  sx={{ justifyContent: "center", mb: 3, gap: 2 }}
+              <RadioGroup
+                row
+                value={period}
+                onChange={(e) =>
+                  setPeriod(e.target.value as "monthly" | "yearly")
+                }
+                sx={{ justifyContent: "center", mb: 4, gap: 2 }}
+              >
+                <FormControlLabel
+                  value="monthly"
+                  control={<Radio />}
+                  label="Monthly"
+                />
+                <FormControlLabel
+                  value="yearly"
+                  control={<Radio />}
+                  label="Annual"
+                />
+              </RadioGroup>
+
+              <FormControl fullWidth sx={{ mb: 3 }}>
+                <InputLabel id="drivers-count-label">
+                  Number of Drivers
+                </InputLabel>
+                <Select
+                  labelId="drivers-count-label"
+                  value={driversCount}
+                  label="Number of Drivers"
+                  onChange={(e) => setDriversCount(e.target.value as number)}
+                  renderValue={(value) => `${value} drivers`}
                 >
-                  <FormControlLabel
-                    value="monthly"
-                    control={<Radio />}
-                    label="Monthly"
-                  />
-                  <FormControlLabel
-                    value="yearly"
-                    control={<Radio />}
-                    label="Annual"
-                  />
-                </RadioGroup>
+                  {Array.from({ length: 49 }, (_, i) => i + 2).map((num) => (
+                    <MenuItem key={num} value={num}>
+                      {num}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
 
-                <FormControl fullWidth sx={{ mb: 3 }}>
-                  <InputLabel id="drivers-count-label">
-                    Number of Drivers
-                  </InputLabel>
-                  <Select
-                    labelId="drivers-count-label"
-                    value={driversCount}
-                    label="Number of Drivers"
-                    onChange={(e) => setDriversCount(e.target.value as number)}
-                    renderValue={(value) => `${value} drivers`}
-                  >
-                    {Array.from({ length: 49 }, (_, i) => i + 2).map((num) => (
-                      <MenuItem key={num} value={num}>
-                        {num}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-
-                <Box sx={{ textAlign: "center", mb: 3 }}>
-                  <Typography variant="h5" component="p" sx={{ mb: 2 }}>
-                    {priceValue}
+              <Divider sx={{ my: 2 }} />
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Typography variant="h6">Total</Typography>
+                <Box sx={{ textAlign: "right" }}>
+                  <Typography variant="h6" color="primary">
+                    {priceValue} / {prices[period].title}
                   </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {prices[period].title}
-                  </Typography>
+                  {prices[period].subtitle && (
+                    <Typography variant="body2" color="text.secondary">
+                      {prices[period].subtitle}
+                    </Typography>
+                  )}
                 </Box>
               </Box>
+            </CardContent>
+          </Card>
+
+          <Card sx={{ maxWidth: 500, mb: 4, width: "100%" }}>
+            <CardContent>
+              <Typography variant="h5" gutterBottom>
+                Payment Details
+              </Typography>
+              <Divider sx={{ mt: 1.5, mb: 2 }} />
 
               <Elements stripe={stripePromise}>
                 <PaymentForm
--- src/app/checkout/single-driver/page.tsx
+++ src/app/checkout/single-driver/page.tsx
@@ -11,6 +11,7 @@ import CardContent from "@mui/material/CardContent";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import Divider from "@mui/material/Divider";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
@@ -25,12 +26,14 @@ const prices = {
   monthly: {
     id: process.env.NEXT_PUBLIC_STRIPE_SINGLE_DRIVER_PRICE_MONTHLY,
     value: "£1.49",
-    title: "Monthly (*Min 3-month term)",
+    title: "Monthly",
+    subtitle: "(*Min 3-month term)",
   },
   yearly: {
     id: process.env.NEXT_PUBLIC_STRIPE_SINGLE_DRIVER_PRICE_YEARLY,
     value: "£15.99",
     title: "Annual",
+    subtitle: "",
   },
 };
 
@@ -89,84 +92,94 @@ export default function SingleDriverPage() {
           sx={{
             my: 4,
             display: "flex",
+            flexDirection: { xs: "column", md: "row" },
             justifyContent: "center",
-            alignItems: "center",
             width: "100%",
             gap: 4,
           }}
         >
-          <Box
-            sx={{
-              mb: 4,
-              maxWidth: 400,
-              width: "100%",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant="h6" component="h2" sx={{ mb: 0.5 }}>
-              eLearning modules:
-            </Typography>
-            <Typography variant="body1" component="div">
-              • Motorway driving
-              <br />
-              • Rural driving
-              <br />
-              • Urban driving
-              <br />
-              • Speed awareness
-              <br />
-              • Fatigue
-              <br />
-              • Share the Road
-              <br />
-              • Eco-driving (good for fossil fuel or alternatively powered
-              vehicles)
-              <br />• Parking and slow-speed manoeuvring
-            </Typography>
-          </Box>
-
-          <Card sx={{ maxWidth: 500, mb: 4, width: "100%" }}>
+          <Card sx={{ maxWidth: 500, mb: 4, width: "100%", height: "100%" }}>
             <CardContent>
+              <Typography variant="h5" gutterBottom>
+                Plan Summary
+              </Typography>
+              <Divider sx={{ my: 1.5 }} />
+
               <Typography
                 variant="h6"
                 component="h2"
                 sx={{ mb: 0.5, textAlign: "center" }}
               >
+                eLearning modules:
+              </Typography>
+              <Typography variant="body1" component="div">
+                • Motorway driving
+                <br />
+                • Rural driving
+                <br />
+                • Urban driving
+                <br />
+                • Speed awareness
+                <br />
+                • Fatigue
+                <br />
+                • Share the Road
+                <br />
+                • Eco-driving (good for fossil fuel or alternatively powered
+                vehicles)
+                <br />• Parking and slow-speed manoeuvring
+              </Typography>
+
+              <Typography
+                variant="h6"
+                component="h2"
+                sx={{ mt: 4, mb: 0.5, textAlign: "center" }}
+              >
                 Select preferable plan
               </Typography>
-              <Box sx={{ mb: 3 }}>
-                <RadioGroup
-                  row
-                  value={period}
-                  onChange={(e) =>
-                    setPeriod(e.target.value as "monthly" | "yearly")
-                  }
-                  sx={{ justifyContent: "center", mb: 3, gap: 2 }}
-                >
-                  <FormControlLabel
-                    value="monthly"
-                    control={<Radio />}
-                    label="Monthly"
-                  />
-                  <FormControlLabel
-                    value="yearly"
-                    control={<Radio />}
-                    label="Annual"
-                  />
-                </RadioGroup>
+              <RadioGroup
+                row
+                value={period}
+                onChange={(e) =>
+                  setPeriod(e.target.value as "monthly" | "yearly")
+                }
+                sx={{ justifyContent: "center", mb: 4, gap: 2 }}
+              >
+                <FormControlLabel
+                  value="monthly"
+                  control={<Radio />}
+                  label="Monthly"
+                />
+                <FormControlLabel
+                  value="yearly"
+                  control={<Radio />}
+                  label="Annual"
+                />
+              </RadioGroup>
 
-                <Box sx={{ textAlign: "center", mb: 3 }}>
-                  <Typography variant="h5" component="p" sx={{ mb: 2 }}>
-                    {prices[period].value}
+              <Divider sx={{ my: 2 }} />
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Typography variant="h6">Total</Typography>
+                <Box sx={{ textAlign: "right" }}>
+                  <Typography variant="h6" color="primary">
+                    {prices[period].value} / {prices[period].title}
                   </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {prices[period].title}
-                  </Typography>
+                  {prices[period].subtitle && (
+                    <Typography variant="body2" color="text.secondary">
+                      {prices[period].subtitle}
+                    </Typography>
+                  )}
                 </Box>
               </Box>
+            </CardContent>
+          </Card>
+
+          <Card sx={{ maxWidth: 500, mb: 4, width: "100%" }}>
+            <CardContent>
+              <Typography variant="h5" gutterBottom>
+                Payment Details
+              </Typography>
+              <Divider sx={{ mt: 1.5, mb: 2 }} />
 
               <Elements stripe={stripePromise}>
                 <PaymentForm
